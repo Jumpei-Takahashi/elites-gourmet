219eed4c3f536447fc4b02249b54f6185d2bf1e8
Jumpei Takahashi <takahashi-junpei@av.avex.co.jp>
2017-01-21 08:31:36 +0000

    01_実習_プロジェクト作成_ログイン画面作成を完了

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  # サムネイル画像にCarrierWaveで作成したUserThumbnailUploaderを使用
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+  # バリデーションを設定/nameの入力は必須/agreementの入力は必須でtrueであること
+  validates :name,      presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
+end
