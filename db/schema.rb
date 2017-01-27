db85e29fb0a1004bf0f57ac5a075af93efafeb36
Jumpei Takahashi <takahashi-junpei@av.avex.co.jp>
2017-01-27 08:37:53 +0000

    07_基本課題を完了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122102322) do
+ActiveRecord::Schema.define(version: 20170124095856) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
@@ -46,8 +46,9 @@ ActiveRecord::Schema.define(version: 20170122102322) do
     t.datetime "open_date"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.text     "restaurant_introduction"
   end
 
   create_table "reviews", force: :cascade do |t|
